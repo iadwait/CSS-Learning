678ecfda23d8237a75d7b108dff5e44ac16a5141
Adwait Barkale <adwaitbarkale@gmail.com>
2025-11-25 14:55:35 +0530

    Worked on flex-grow flex-shrink flex-basis practice

---
+++ cssLearning/src/lib/flexboxLayout/flexLayoutPractice.svelte
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="card">1</div>
+    <div class="card">2</div>
+    <div class="card">3</div>
+    <div class="card">4</div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        gap: 10px;
+        flex-wrap: wrap;
+        border: 1px solid black;
+        padding: 10px;
+    }
+    .card:nth-child(1) {
+        flex: 1 1 10px;
+        background: rgb(167, 94, 94);
+    }
+    .card:nth-child(2) {
+        flex: 1 1 10px;
+        background: rgb(84, 45, 45);
+    }
+    .card:nth-child(3) {
+        flex: 1 2 10px;
+        background: rgb(167, 94, 94);
+    }
+    .card:nth-child(4) {
+        flex: 1 1 10px;
+        background: rgb(147, 62, 62);
+    }
+</style>
--- cssLearning/src/routes/flexboxLayout/+page.svelte
+++ cssLearning/src/routes/flexboxLayout/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import FlexboxLayout from "$lib/flexboxLayout/flexboxLayout.svelte";
+    import FlexLayoutPractice from "$lib/flexboxLayout/flexLayoutPractice.svelte";
 </script>
 
 <h1>FlexBox Layout -> Sidebar with Main Content example</h1>
 <FlexboxLayout />
+ <!-- <FlexLayoutPractice /> -->
